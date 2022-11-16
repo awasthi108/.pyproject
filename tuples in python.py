978e5ff9c850772c91eaedc7c969e1b673d3c878
awasthi108 <79709621+awasthi108@users.noreply.github.com>
2022-11-16 09:24:44 +0530

    Add files via upload
    You can learn tuples by this project.

---
+++ tuples in python.py
@@ -0,0 +1,21 @@
+#creating a tuple using ()
+#you cannot use list tag in tuples
+
+t1 = (2)
+print(t1)#wrong way to make a single tuple
+
+t1 = (1,)
+print(t1)#to make a single tuple
+
+#tuple to count times a element is repeted
+t = (5,55,25,6,89,56,25,96,25)
+
+print(t.count(25))
+
+#to find the index position
+
+t = (5,55,25,6,89,56,25,96,25)
+
+print(t.index(6))
+
+ 
